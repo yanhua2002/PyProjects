5464e2a1c96a9afb984924bd8670d952167b75c3
Xiao Ma <yanhua2002@msn.cn>
2017-08-08 01:47:58 +0800

    Successfully logged in

--- PyZhihuEmoj/PyZhihuEmoj/PyWeiboLoginAPI.py
+++ PyZhihuEmoj/PyZhihuEmoj/PyWeiboLoginAPI.py
@@ -19,6 +19,13 @@ class PyWeiboLoginAPI(object):
         self.session=requests.Session()
         self.session.headers.update({"User-Agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/59.0.3071.115 Safari/537.36"})
 
+    def get_zhihu_token(self):
+        zhihu_home=self.session.get("http://www.zhihu.com")
+        self.csrf_string=zhihu_home.cookies["_xsrf"]
+        redirect_to_sina_res=self.session.get("https://www.zhihu.com/oauth/redirect/login/sina?next=/oauth/account_callback&from=%2F")
+        logging.debug("redirect to sina response: %s",redirect_to_sina_res.status_code)
+        return
+
     def encode_username(self):
         """ encode user name """
 
@@ -35,9 +42,11 @@ class PyWeiboLoginAPI(object):
             "checkpin":"1",
             "client":"ssologin.js(v1.4.18)",
             "_":int(time.time()*1000)}
-        
+        login_referer="https://api.weibo.com/oauth2/authorize?scope=email&state="+self.csrf_string+"redirect_uri=http%3A%2F%2Fwww.zhihu.com%2Foauth%2Fcallback%2Fsina&response_type=code&client_id=3063806388"
         try:
-            response=self.session.get("https://login.sina.com.cn/sso/prelogin.php",params=params)
+            response=self.session.get("https://login.sina.com.cn/sso/prelogin.php",
+                                      params=params,
+                                      headers={"Referer":login_referer})
             logging.debug("response.text: %s",response.text)
             prelogin_data=json.loads(re.search(r"\((?P<data>.*)\)", response.text).group("data"))
         except Exception as excep:
@@ -59,8 +68,9 @@ class PyWeiboLoginAPI(object):
         self.user_name=user_name
         self.pass_word=pass_word
 
+        self.get_zhihu_token()
+
         login_referer="https://api.weibo.com/oauth2/authorize?scope=email&state="+self.csrf_string+"redirect_uri=http%3A%2F%2Fwww.zhihu.com%2Foauth%2Fcallback%2Fsina&response_type=code&client_id=3063806388"
-        self.session.headers.update({"Referer":login_referer})
 
         s_user_name=self.encode_username()
         logging.debug("s_user_name: %s",s_user_name)
@@ -104,7 +114,9 @@ class PyWeiboLoginAPI(object):
             post_data["door"]=code
 
         login_url="https://login.sina.com.cn/sso/login.php?client=ssologin.js(v1.4.18)&_=%d&openapilogin=qrcode" % int(time.time())
-        login_res_data=self.session.post(login_url,data=post_data).json()
+        login_res_data=self.session.post(login_url,
+                                         data=post_data,
+                                         headers={"Referer":login_referer}).json()
         logging.debug("login_res_data: %s",login_res_data)
         if login_res_data["retcode"]=="0":
             auth1_post_data={
@@ -127,7 +139,9 @@ class PyWeiboLoginAPI(object):
                 "switchLogin":"null",
                 "userId":"",
                 "passwd":""}
-            auth1_res=self.session.post("https://api.weibo.com/oauth2/authorize",data=auth1_post_data)
+            auth1_res=self.session.post("https://api.weibo.com/oauth2/authorize",
+                                        data=auth1_post_data,
+                                        headers={"Referer":login_referer})
             logging.debug("auth1_res.status_code: %s",auth1_res.status_code)
             """auth2_form=re.search(r"<form name=\"authZForm\".*?\/form>",auth1_res.text)"""
             """auth2_form=re.findall(r"<input type=\"hidden\".*?\/>",auth1_res.text)"""
@@ -139,22 +153,23 @@ class PyWeiboLoginAPI(object):
             logging.debug("auth2_post_data: %s",auth2_post_data)
             auth2_res=self.session.post("https://api.weibo.com/oauth2/authorize",
                                         data=auth2_post_data,
-                                        allow_redirects=False,
                                         headers={"Referer":"https://api.weibo.com/oauth2/authorize",
                                                  "Origin":"https://api.weibo.com",
                                                  "Host":"api.weibo.com",
                                                  "Upgrade-Insecure-Requests":"1",
                                                  "Content-Type":"application/x-www-form-urlencoded"})
             logging.debug("auth2_res.status_code: %s",auth2_res.status_code)
-            if auth2_res.status_code==302:
-                redirecturl=auth2_res.headers["Location"].replace("http:","https:")
+            if auth2_res.status_code==502:
+                redirecturl=auth2_res.history[0].headers["Location"].replace("http:","https:")
                 redirect_res=self.session.get(redirecturl,
                                               headers={"Host":"www.zhihu.com",
                                                        "Upgrade-Insesure-Requests":"1"})
                 logging.debug("redirect_res.text: %s",redirect_res.text)
+                zhihu_home_loggedin=self.session.get("https://www.zhihu.com",
+                                                     headers={"Host":"www.zhihu.com",
+                                                              "Referer":redirecturl,})
+                logging.debug("zhihu homepage loggedin: %s",zhihu_home_loggedin.status_code)
         return
 
 weibo=PyWeiboLoginAPI()
-zhihu_csrf=weibo.session.get("http://www.zhihu.com")
-weibo.csrf_string=zhihu_csrf.cookies["_xsrf"]
 weibo.login("13793288171","husu0301")
